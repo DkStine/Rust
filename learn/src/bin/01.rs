701c071568fdfe071b29c1054ab900d390281227
Deepak_Kumar <dkctcak@gmail.com>
2024-04-07 18:09:16 +0530

    Creating functions

--- learn/src/bin/01.rs
+++ learn/src/bin/01.rs
@@ -1,11 +1,30 @@
+/* 
 fn first_name() {
     print!("Deepak ");
 }
 fn last_name() {
-    print!("Kumar");
+    println!("Kumar");
+}
+*/
+
+// a2
+fn add(a: i32, b: i32) -> i32 {
+    a + b
 }
 
+fn display_result(result: i32) {
+    println!("{:?}", result);
+}
 fn main() {
+    // a1
+    /* 
     first_name();
     last_name();
+    */
+
+    // a2
+    let result = add(45, 32);
+    display_result(result);
+
+    
 }
